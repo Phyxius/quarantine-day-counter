cab01b5362c286646bfa5ce25f261f11b76bac60
Shea Polansky <shea@polansky.co>
2021-01-03 20:30:49 -0800

    Add weather display

--- code.py
+++ code.py
@@ -3,14 +3,25 @@ import board
 import terminalio
 from adafruit_matrixportal.matrixportal import MatrixPortal
 from secrets import secrets
+import rtc
 
 TEXT_COLOR = 0x202020
 TIME_UPDATE_INTERVAL = 1 * 60 * 60
+OPENWEATHER_API_URL = f"https://api.openweathermap.org/data/2.5/weather?q={secrets['zip']}&appid={secrets['openweather_api_key']}&units={secrets.get('units', 'imperial')}"
+TEMP_PATH = ["main", "temp"]
 
 matrixportal = MatrixPortal(
     status_neopixel=board.NEOPIXEL,
     bit_depth = 4
 )
+
+weather = None
+
+def refresh_data():
+    global weather
+    temp = matrixportal.network.fetch_data(OPENWEATHER_API_URL, json_path=(TEMP_PATH))[0]
+    weather = int(temp)
+    matrixportal.get_local_time()
 
 matrixportal.add_text(
     text_font=terminalio.FONT,
@@ -27,7 +38,8 @@ matrixportal.add_text(
 matrixportal.set_text("Connecting", 1)
 matrixportal.network.connect()
 matrixportal.set_text("Get time", 1)
-matrixportal.get_local_time()
+# matrixportal.get_local_time()
+refresh_data()
 
 last_time_refresh = time.time()
 
@@ -35,7 +47,6 @@ epoch = 1584576000 #march 19, 2020 00:00 UTC
 seconds_per_day = 24 * 60 * 60
 
 days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-# months = ['Jan', 'Feb', 'March', 'Apr', 'May', 'June', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
 matrixportal.preload_font(b"012345789Quarantinedy")  # preload numbers
 while True: 
@@ -47,14 +58,18 @@ while True:
     first_line = f"{hour:02}:{(dt.tm_min):02}"
     day_of_week = days[dt.tm_wday]
     date = ""
-    if t % 60 >= 30:
+    data_selector = t % 90
+    if data_selector >= 60:
         day = (t - epoch) // seconds_per_day
         date = f"QD {day}"
+    elif data_selector >= 30:
+        date = f"{weather}F"
     else:
         date = f"{dt.tm_mon:02}/{dt.tm_mday:02}"
     matrixportal.set_text(first_line, 0)
     matrixportal.set_text(f"{day_of_week} {date}", 1)
     if (t - last_time_refresh > TIME_UPDATE_INTERVAL):
-        matrixportal.get_local_time()
+        refresh_data()
+        # matrixportal.get_local_time()
         last_time_refresh = t
     time.sleep(0.05)
--- secrets.py
+++ secrets.py
@@ -3,5 +3,7 @@ secrets = {
     'password': 'YOUR_PASSWORD',
     'timezone': 'America/Los_Angeles',
     'aio_username': 'YOUR_USERNAME',
-    'aio_key': 'YOUR_KEY'
+    'aio_key': 'YOUR_KEY',
+    'zip_code': 123456,
+    'openweather_api_key': 'YOUR_OPENWEATHER_API_KEY'
 }
