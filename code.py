2ea1ae376c5a88dab955d37beea9b4cdfe748eff
Shea Polansky <shea@polansky.co>
2021-02-17 19:06:11 -0800

    Reboot on error

--- code.py
+++ code.py
@@ -3,7 +3,7 @@ import board
 import terminalio
 from adafruit_matrixportal.matrixportal import MatrixPortal
 from secrets import secrets
-import rtc
+import microcontroller
 
 TEXT_COLOR = 0x202020
 TIME_UPDATE_INTERVAL = 1 * 60 * 60
@@ -49,27 +49,30 @@ seconds_per_day = 24 * 60 * 60
 days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 
 matrixportal.preload_font(b"012345789Quarantinedy")  # preload numbers
-while True: 
-    t = time.time()
-    dt = time.localtime(t)
-    hour = dt.tm_hour
-    if hour == 0: hour = 12
-    elif hour > 12: hour -= 12
-    first_line = f"{hour:02}:{(dt.tm_min):02}"
-    day_of_week = days[dt.tm_wday]
-    date = ""
-    data_selector = t % 90
-    if data_selector >= 60:
-        day = (t - epoch) // seconds_per_day
-        date = f"QD {day}"
-    elif data_selector >= 30:
-        date = f"{weather}F"
-    else:
-        date = f"{dt.tm_mon:02}/{dt.tm_mday:02}"
-    matrixportal.set_text(first_line, 0)
-    matrixportal.set_text(f"{day_of_week} {date}", 1)
-    if (t - last_time_refresh > TIME_UPDATE_INTERVAL):
-        refresh_data()
-        # matrixportal.get_local_time()
-        last_time_refresh = t
-    time.sleep(0.05)
+try:
+    while True: 
+        t = time.time()
+        dt = time.localtime(t)
+        hour = dt.tm_hour
+        if hour == 0: hour = 12
+        elif hour > 12: hour -= 12
+        first_line = f"{hour:02}:{(dt.tm_min):02}"
+        day_of_week = days[dt.tm_wday]
+        date = ""
+        data_selector = t % 90
+        if data_selector >= 60:
+            day = (t - epoch) // seconds_per_day
+            date = f"QD {day}"
+        elif data_selector >= 30:
+            date = f"{weather}F"
+        else:
+            date = f"{dt.tm_mon:02}/{dt.tm_mday:02}"
+        matrixportal.set_text(first_line, 0)
+        matrixportal.set_text(f"{day_of_week} {date}", 1)
+        if (t - last_time_refresh > TIME_UPDATE_INTERVAL):
+            refresh_data()
+            # matrixportal.get_local_time()
+            last_time_refresh = t
+        time.sleep(0.05)
+except e:
+    microcontroller.reset()
